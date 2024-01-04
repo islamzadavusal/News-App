c2111293f340f3b7325f0bd0e63dcf241f324816
islamzada <vusal.islamzada.01@gmail.com>
2024-01-04 20:41:01 +0400

    Four

--- app/src/main/java/com/islamzada/newsapp/ui/adapter/MainAdapter.kt
+++ app/src/main/java/com/islamzada/newsapp/ui/adapter/MainAdapter.kt
@@ -3,6 +3,7 @@ package com.islamzada.newsapp.ui.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -12,96 +13,96 @@ import com.islamzada.newsapp.databinding.ItemNewsBinding
 import dagger.hilt.android.qualifiers.ActivityContext
 import javax.inject.Inject
 
-class MainAdapter @Inject constructor(@ActivityContext private val context : Context): RecyclerView.Adapter<MainAdapter.MyViewHolder>()
-{
-    private lateinit var binding : ItemNewsBinding
+class MainAdapter @Inject constructor(@ActivityContext private val context: Context) :
+    RecyclerView.Adapter<MainAdapter.MyViewHolder>() {
+
     private var newsList = emptyList<Article>()
 
-    inner class MyViewHolder : RecyclerView.ViewHolder(binding.root)
-    {
-        fun setData(data:Article)
-        {
+    // Inner class: RecyclerView.ViewHolder'ın özelleştirilmiş sürümü
+    inner class MyViewHolder(private val binding: ItemNewsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        // Verileri ViewHolder'a atayan fonksiyon
+        fun setData(data: Article) {
             binding.apply {
+                // Veri bağlama işlemleri
                 newsTitle.text = data.title
                 newsDescription.text = shortenString(data.description)
-                Glide.with(context)
-                    .load(data.imageUrl)
-                    .error(R.drawable.ic_news)
-                    .placeholder(R.drawable.ic_news)
-                    .into(newsImage)
+                loadImage(data.imageUrl, newsImage)
             }
         }
     }
-    fun shortenString(input : String?):String{
-        if (input != null){
-            return if (input.length >100)
-            {
-                val shortenedText = input.substring(0,100)
-                val lastSpaceIndex = shortenedText.lastIndexOf(' ')
-                if (lastSpaceIndex != -1)
-                {
-                    shortenedText.substring(0,lastSpaceIndex)+" ..."
-                }else {
-                    "$shortenedText ..."
-                }
-            }else{
-                input
-            }
-        }else {
-            return ""
-        }
+
+    // ViewHolder'ı oluşturan fonksiyon
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        // View oluşturucu
+        val inflater = LayoutInflater.from(context)
+        val binding = ItemNewsBinding.inflate(inflater, parent, false)
+        return MyViewHolder(binding)
     }
 
-    override fun onCreateViewHolder(parent : ViewGroup, viewType : Int) : MyViewHolder
-    {
-        binding = ItemNewsBinding.inflate(LayoutInflater.from(context),parent,false)
-        return MyViewHolder()
-    }
-
-    override fun getItemCount() : Int
-    {
+    // Veri setinin boyutunu döndüren fonksiyon
+    override fun getItemCount(): Int {
         return newsList.size
     }
 
-    override fun getItemViewType(position : Int) : Int
-    {
+    // Belirli bir konumdaki öğenin türünü döndüren fonksiyon
+    override fun getItemViewType(position: Int): Int {
         return position
     }
 
-    override fun onBindViewHolder(holder : MyViewHolder , position : Int)
-    {
+    // ViewHolder'ın içeriğini belirli bir konumdaki veriyle güncelleyen fonksiyon
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         holder.setData(newsList[position])
     }
-    fun submitData(data : List<Article>)
-    {
-        val newsDiffUtil = NewsDiffUtils(newsList,data)
-        val diffutils = DiffUtil.calculateDiff(newsDiffUtil)
+
+    // Veri setini güncelleyen fonksiyon
+    fun submitData(data: List<Article>) {
+        val newsDiffUtil = NewsDiffUtils(newsList, data)
+        val diffUtils = DiffUtil.calculateDiff(newsDiffUtil)
         newsList = data
-        diffutils.dispatchUpdatesTo(this)
+        diffUtils.dispatchUpdatesTo(this)
     }
+
+    // Resmi yükleyen Glide kütüphanesi kullanılan yardımcı fonksiyon
+    private fun loadImage(imageUrl: String?, imageView: ImageView) {
+        Glide.with(context)
+            .load(imageUrl)
+            .error(R.drawable.ic_news)
+            .placeholder(R.drawable.ic_news)
+            .into(imageView)
+    }
+
+    // String'i kısaltan yardımcı fonksiyon
+    private fun shortenString(input: String?): String {
+        return input?.let {
+            if (it.length > 100) {
+                val shortenedText = it.substring(0, 100)
+                val lastSpaceIndex = shortenedText.lastIndexOf(' ')
+                if (lastSpaceIndex != -1) {
+                    "${shortenedText.substring(0, lastSpaceIndex)} ..."
+                } else {
+                    "$shortenedText ..."
+                }
+            } else {
+                it
+            }
+        } ?: ""
+    }
+
+    // RecyclerView için DiffUtil sınıfı
     class NewsDiffUtils(
-        private val oldItem : List<Article>,
-        private val newItem : List<Article>
-    ) : DiffUtil.Callback()
-    {
-        override fun getOldListSize() : Int
-        {
-            return oldItem.size
-        }
+        private val oldItems: List<Article>,
+        private val newItems: List<Article>
+    ) : DiffUtil.Callback() {
+        override fun getOldListSize(): Int = oldItems.size
 
-        override fun getNewListSize() : Int
-        {
-            return newItem.size
-        }
+        override fun getNewListSize(): Int = newItems.size
 
-        override fun areItemsTheSame(oldItemPosition : Int , newItemPosition : Int) : Boolean
-        {
-            return oldItem[oldItemPosition] === newItem[newItemPosition]
-        }
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldItems[oldItemPosition] === newItems[newItemPosition]
 
-        override fun areContentsTheSame(oldItemPosition : Int , newItemPosition : Int) : Boolean
-        {
-            return oldItem[oldItemPosition] === newItem[newItemPosition]
-        }
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+            oldItems[oldItemPosition] == newItems[newItemPosition]
     }
 }
--- app/src/main/java/com/islamzada/newsapp/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/islamzada/newsapp/ui/fragment/MainFragment.kt
@@ -5,19 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.fragment.app.viewModels
 import com.islamzada.newsapp.databinding.FragmentMainBinding
 import com.islamzada.newsapp.ui.adapter.MainAdapter
 import com.islamzada.newsapp.ui.viewModel.MainViewModel
+import com.islamzada.newsapp.utils.MyResponse
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
-    private var _binding: FragmentMainBinding? = null
-    private val binding get() = _binding
 
+    private lateinit var binding: FragmentMainBinding
     private val viewModel: MainViewModel by viewModels()
 
     @Inject
@@ -26,46 +27,47 @@ class MainFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentMainBinding.inflate(inflater, container, false)
-        return binding?.root
+    ): View {
+
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Görünümlerin yapılandırılması
         setupViews()
-//        observeNewsData()
+
+        // Haber verilerinin gözlemlenmesi
+        observeNewsData()
+
         viewModel.getAllNotes()
     }
 
-//    private fun observeNewsData() {
-//        viewModel.newsData.observe(viewLifecycleOwner) { response ->
-//            when (response.status) {
-//                LOADING -> {
-//                    binding?.loading?.visibility = View.VISIBLE
-//                }
-//                SUCCESS -> {
-//                    binding?.loading?.visibility = View.GONE
-//                    response?.data?.articles?.let { newsAdapter.submitData(it) }
-//                }
-//                ERROR -> {
-//                    binding?.loading?.visibility = View.GONE
-//                    Toast.makeText(requireContext(), response.message, Toast.LENGTH_SHORT).show()
-//                }
-//            }
-//        }
-//    }
-
-    private fun setupViews() {
-        binding?.recyclerview?.apply {
-            layoutManager =
-                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-            adapter = newsAdapter
+    private fun observeNewsData() {
+        viewModel.newsData.observe(viewLifecycleOwner) { response ->
+            when (response.status) {
+                MyResponse.Status.LOADING -> {
+                    binding.loading.visibility = View.VISIBLE
+                }
+                MyResponse.Status.SUCCESS -> {
+                    binding.loading.visibility = View.GONE
+                    response.data?.articles?.let { newsAdapter.submitData(it) }
+                }
+                MyResponse.Status.ERROR -> {
+                    binding.loading.visibility = View.GONE
+                    Toast.makeText(requireContext(), response.message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun setupViews() {
+        binding.recyclerview.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = newsAdapter
+        }
     }
 }
+
